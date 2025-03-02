2ec656139e500b4f0e59276dd796fd47573d0352
avamus <lukassys2004@gmail.com>
2025-03-02 17:51:36 +0000

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import React, { useState } from 'react'
+import { TeamPsychologyDashboard } from '@/components/custom/TeamPsychologyDashboard'
+
+export default function TeamPsychologyPage() {
+  return <TeamPsychologyDashboard />
+}
