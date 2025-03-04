1b96233d3a9065745db94fc7c4d3aeeef6c7ccf6
avamus <lukassys2004@gmail.com>
2025-03-04 22:43:28 +0000

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,8 +1,17 @@
 "use client"
 
-import React, { useState } from 'react'
-import { TeamPsychologyDashboard } from '@/components/custom/TeamPsychologyDashboard'
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
+import { VisionDashboardManager } from '@/components/ui/vision-dashboard-manager'
 
-export default function TeamPsychologyPage() {
-  return <TeamPsychologyDashboard />
+export default function Home() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <main className="min-h-screen bg-white">
+        <div className="max-w-[1400px] mx-auto">
+          <VisionDashboardManager />
+        </div>
+      </main>
+    </DndProvider>
+  )
 }
